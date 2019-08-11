1d5f23ba462d108da81f273997ad06733c12117f
Chris Simpson <chris.m.simpson@icloud.com>
2019-08-12 08:39:33 +1000

    Add a bunch of icons

---
+++ Source/Neukolln_Light_autospace.py
@@ -0,0 +1,10 @@
+
+# Reference
+# Script, Category, Subcategory, value, referenceGlyph, filter
+
+# Letters
+*,Letter,Uppercase,1.25,H,*,
+*,Letter,Lowercase,1,x,*,
+
+# Numbers
+*,Number,Decimal Digit,1.2,one,*,
